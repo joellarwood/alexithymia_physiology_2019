3c2e638d44f4fbeed6871e77d9cf5306cf3ee92b
Joel Larwood <joel.larwood@uq.net.au>
2020-06-10 15:50:08 +1000

    create data cleaning function

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-alexithymia_physiology_20119
+# Physiological Responses to Music in Alexithymia
+
+[![Creative Commons License](https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png)](http://creativecommons.org/licenses/by-nc-sa/4.0/)<br>
+This work is licensed under a [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-nc-sa/4.0/).
+
+Here I provide the experiment code used for the study for the purpose of replication. I additionally provide the data and code that was analysed for the resultant paper [doi to be added].
+
+--------------------------------------------------------------------------------
+
+The study required participants to listen to a series of musical clips for 30 seconds each. Each clip was presented twice and participants rated the valence of the clip, the arousal of the clip, how much they liked the clip, and the extent to which a certain emotions where experienced during listening. Electrophysiological data was collected from the zygomaticus and corugator using facial EMG as well as electrodermal activity. This data was collected continuously with the beginning of each song marked by a event marker.
---
+++ data_cleaning/clean_psychopy.R
@@ -0,0 +1,187 @@
+#################################################################
+##                  Processing psychopy files                  ##
+#################################################################
+
+# Load required packages --------------------------------------------------
+library(magrittr) # get pipe 
+library(readr) # to laod data
+library(dplyr) # to manipulate data
+library(tidyr) # to tidy data
+library(visdat) # to visualise data
+library(janitor) # clean names
+
+
+# Process single file -----------------------------------------------------
+
+raw <- here::here(
+  "data", 
+  "individual_psychopy_data", 
+  "001_MusicAffectPsychophysiology2019_2019_May_07_0907.csv"
+) %>% 
+  readr::read_csv() %>% 
+  janitor::clean_names() %>% 
+  tidyr::drop_na(
+    song
+  )
+  
+
+glimpse(raw)
+
+raw_shrink <- raw %>% 
+  select( #keep required variables
+   participant, 
+   song, 
+   valence,
+   arousal, 
+   affectcat,
+   songmark,
+   word,
+   songtrials_this_trial_n,
+   songtrials_this_rep_n,
+   songtrials_this_n,
+   arousalkey_keys,
+   valencekey_keys,
+   likekey_keys,
+   word_response_keys
+  ) %>% 
+  rename( # make names more clear
+    valence_target = valence, 
+    arousal_target = arousal, 
+    rep_number = songtrials_this_rep_n,
+    trial_number = songtrials_this_n,
+    arousal_rating = arousalkey_keys,
+    valence_rating = valencekey_keys,
+    liking = likekey_keys,
+    emotion_rating = word_response_keys
+  ) %>% 
+  mutate( # code factors
+    valence_target = recode_factor(
+      valence_target, 
+      "1" = "negative", 
+      "2" = "positive"
+    ), 
+    arousal_target = recode_factor(
+      arousal_target, 
+      "1" = "low", 
+      "2" = "high"
+    ), 
+    emotion_target = recode_factor(
+      affectcat, 
+      "1" = "happy", 
+      "2" = "fear", 
+      "3" = "sad",
+      "4" = "tender"
+    )
+  )
+)
+
+raw_spread <- raw_shrink %>% 
+  tidyr::pivot_wider( # pivot wider so that each song has only one row with emotion rating as column
+    id_cols = trial_number, 
+    names_from = word,
+    values_from = emotion_rating
+  ) %>% 
+  janitor::clean_names() 
+
+raw_valence_arousal <- raw_shrink %>% 
+  drop_na(valence_rating)
+
+
+processed <- left_join(
+  raw_valence_arousal, 
+  raw_spread, 
+  by = "trial_number"
+)
+
+
+
+# Create function ---------------------------------------------------------
+
+psychopy_process <- function(x) {
+  # import and clean file
+  tmp_data <- read_csv(x) %>% # import file
+    janitor::clean_names() %>% # make name snake case
+    tidyr::drop_na( # drop practice trial
+      song
+    ) %>%
+    select( # keep required variables
+      participant,
+      song,
+      valence,
+      arousal,
+      affectcat,
+      songmark,
+      word,
+      songtrials_this_trial_n,
+      songtrials_this_rep_n,
+      songtrials_this_n,
+      arousalkey_keys,
+      valencekey_keys,
+      likekey_keys,
+      word_response_keys
+    ) %>%
+    rename( # make names more clear
+      valence_target = valence,
+      arousal_target = arousal,
+      rep_number = songtrials_this_rep_n,
+      trial_number = songtrials_this_n,
+      arousal_rating = arousalkey_keys,
+      valence_rating = valencekey_keys,
+      liking = likekey_keys,
+      emotion_rating = word_response_keys
+    ) %>%
+    mutate( # code factors
+      valence_target = recode_factor(
+        valence_target,
+        "1" = "negative",
+        "2" = "positive"
+      ),
+      arousal_target = recode_factor(
+        arousal_target,
+        "1" = "low",
+        "2" = "high"
+      ),
+      emotion_target = recode_factor(
+        affectcat,
+        "1" = "happy",
+        "2" = "fear",
+        "3" = "sad",
+        "4" = "tender"
+      )
+    )
+
+  tmp_spread <- tmp_data %>%
+    tidyr::pivot_wider( # pivot wider so that each song has only one row with emotion rating as column
+      id_cols = trial_number,
+      names_from = word,
+      values_from = emotion_rating
+    ) %>%
+    janitor::clean_names() # clean names of emotion words
+
+  tmp_v_a <- tmp_data %>%
+    drop_na(valence_rating) # get only row with affect ratings
+
+
+  tmp_processed <- left_join(
+    tmp_v_a,
+    tmp_spread,
+    by = "trial_number"
+  )
+}
+
+
+
+# test_function -----------------------------------------------------------
+
+test <- here::here(
+  "data", 
+  "individual_psychopy_data", 
+  "001_MusicAffectPsychophysiology2019_2019_May_07_0907.csv"
+) %>% 
+  psychopy_process()
+
+identical(test, processed)
+
+visdat::vis_dat(test)
+
+
